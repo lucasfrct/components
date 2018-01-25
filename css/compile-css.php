a7168aa9224035165e141fb66afb2fc6d380f48d
Lucas Costa <lucasfrct@gmail.com>
2018-01-25 15:07:42 -0200

    Atualização do file reset

--- css/compile-css.php
+++ css/compile-css.php
@@ -1,16 +1,19 @@
 <?php 
 include ( "../php/Compile.php" );
 
-echo '<meta charset="utf-8"><meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">';
+header ( "Content-type: text/css" );
+#echo '<meta charset="utf-8"><meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">';
 
 $componentsCss = array (
 	'src/component-reset.css',
+	'src/component-line.css',
+	'src/component-grid-col-xs.css',
 );
 
 Compile::list ( $componentsCss );
 Compile::join ( );
 if ( Compile::write ( "all-components.css" ) ) {
-	echo '<meta http-equiv="refresh" content="0; URL=..\css.html">';
+	#echo '<meta http-equiv="refresh" content="0; URL=..\css.html">';
 } else {
 	Compile::report ( );
 };
