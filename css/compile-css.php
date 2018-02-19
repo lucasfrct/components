9c9418555b3248529e7fa2ca0c1e1e66854df2a5
Lucas Costa <lucasfrct@gmail.com>
2018-02-19 20:51:45 -0300

    bug of margin in row-

--- css/compile-css.php
+++ css/compile-css.php
@@ -1,15 +1,17 @@
 <?php 
-include ( "../php/Compile.php" );
+include_once ( "../php/Compile.php" );
 header ( "Content-type: text/css" );
 
 $componentsCss = array (
 	'src/component-reset.css',
 	'src/component-line.css',
+	'src/component-grid-col-all.css',
 	'src/component-grid-col-xs.css',
 	'src/component-grid-col-sm.css',
 	'src/component-grid-col-md.css',
 	'src/component-grid-col-lg.css',
 	'src/component-grid-col-xl.css',
+	'src/component-grid-row-all.css',
 	'src/component-grid-row-xs.css',
 	'src/component-grid-row-sm.css',
 	'src/component-grid-row-md.css',
@@ -39,11 +41,13 @@ if ( !Compile::write ( "all-components.css" ) ) {
 $componentsGrid = array (
 	'src/component-reset.css',
 	'src/component-line.css',
+	'src/component-grid-col-all.css',
 	'src/component-grid-col-xs.css',
 	'src/component-grid-col-sm.css',
 	'src/component-grid-col-md.css',
 	'src/component-grid-col-lg.css',
 	'src/component-grid-col-xl.css',
+	'src/component-grid-row-all.css',
 	'src/component-grid-row-xs.css',
 	'src/component-grid-row-sm.css',
 	'src/component-grid-row-md.css',
