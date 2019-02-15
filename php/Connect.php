aa2b93ab496cadffef500aaa4e58572c7547bd01
Lucas Costa <lucasfrct@users.noreply.github.com>
2019-02-14 23:56:36 -0200

    add insert and Select in Connect

--- php/Connect.php
+++ php/Connect.php
@@ -3,58 +3,111 @@
 
 class Connect 
 {
-	# Instância única 
-	private static $instance = null;
-	private $database = "";
-	private $connection = null;
+	# Singleton Instance
+	private static $instance = null; # Instância única
+	private $database = ""; # nome o vanco de dados
+	private $connection = null; # instância do PDO connect
 
-	const HOST = "127.0.0.1";
-	const USERNAME = "root";
-	const PASSWORD = "";
+	const HOST = "127.0.0.1:3306"; # servidor mysql
+	const USERNAME = "root"; # usuário de acesso
+	const PASSWORD = ""; # senha passe de acesso
 
-	private function ping ( string $domain = "127.0.0.1" ) {
-		$status = FALSE;
-		exec ( "ping -n 1 -w 1 " . $domain, $output, $result );
-		var_dump ( $output ); 
-		$status =  ( !$result && count ( $output ) > 2 ) ? TRUE : FALSE;
-		return $status;
+	# faz um pingo no servidor 
+	private function ping ( string $host = "127.0.0.1:80" ) 
+	{
+		$server = @fsockopen ( $host, -1, $errCode, $errStr, 1 );
+		@fclose ( $server );
+		return ( $server ) ? TRUE : FALSE;
 	}
 
+	# método stático para iniciar a classe em sigleton
 	public static function on ( string $database = "" ): Connect
 	{
-		if ( null == self::$instance ) {
+		# cria uma nova instância
+		if ( null == self::$instance && !empty ( $database ) ) {
 			self::$instance = new Connect;
 		};
 		
-		self::$instance->database = $database;
-		
-		if ( self::ping ( self::HOST ) ) {
-			self::$instance->getConnection ( );
+		if ( !empty ( $database ) ) {
+			# carrega o nome do banco de dados
+			self::$instance->database = $database;
+			
+			# inicia a conexão PDO
+			if ( self::$instance->ping ( self::HOST ) ) {
+				self::$instance->getConnection ( );
+			};
 		};
-
-		echo self::ping ( self::HOST );
 		
+		#retorna um objeto tipo Connect
 		return self::$instance;
 	}
 
-	private function getConnection ( ): PDO 
+	# faz a connexão como o banco de dados PDO
+	private function getConnection ( ): PDO
 	{
 		if ( null == $this->connection ) {
-			$this->connection = @new \PDO ( 
-				"mysql:dbname=".self::$instance->database.";host:".self::HOST."", 
+			$this->connection = @new PDO ( 
+				"mysql:dbname=".$this->database.";host:".self::HOST."", 
 				self::USERNAME, 
 				self::PASSWORD 
 			);
-		};
+			# habilita o o error de exceção
+			$this->connection->setAttribute ( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+			# configura o Charset para UTF-8
+			$this->connection->exec("set names utf8");
 
-		return $this->connection;
+			return $this->connection;	
+		};
 	}
 
-	public function query ( string $sql = "", $args = Array ( ) )
+	# inicia um acesso  ao bacno de dados
+	public function query ( string $sql = "", array $data = Array ( ) )
 	{
 		$stmt = $this->connection->prepare ( $sql );
-		$stmt->execute ( $args );
+		$stmt->execute ( $data );
 		return $stmt; 
+	}
+
+	public function insert ( string $table = "", string $fields = "*", string $values = "" ): bool
+	{
+		$params = explode ( ",", $fields );
+		$params = array_map ( function ( $item  ) {
+			return  ":".trim( $item );
+		}, $params );
+		$paramsStr = implode ( ", ", $params );
+		
+		preg_match_all ( '/\'([^\']*)\'/', $values, $result );
+		$values  = $result [ 1 ];
+
+		$data = Array ( );
+		foreach ( $params as $key => $field ) {
+			$data = array_merge ( $data, array ( $field => $values [ $key ] ) );
+		};
+
+		$sql = "INSERT INTO {$table} ({$fields}) VALUES({$paramsStr})";
+		$insert = $this->query ( $sql, $data );
+		return  ( $insert->rowCount ( ) > 0 ) ? TRUE : FALSE;
+	}
+
+	public function select ( string  $table = "", string $fields = "", string $condition = "" ): array
+	{
+		#$sql = "SELECT {$fields} FROM {$table } WHERE {$condition}";
+		$condition = ( !empty ( $condition ) ) ? " WHERE {$condition}" : "";
+		$sql = "SELECT {$fields} FROM {$table } {$condition}";
+		$consult = $this->query ( $sql );
+		$result = Array ( );
+		while ( $row = $consult->fetch(PDO::FETCH_ASSOC) ) {
+			$result = array_merge ( $result, array ( $row ) );
+		};
+		return $result;
+	}
+
+	public function update ( string $table = "", string $data = "" , string $condition = "" ): bool
+	{
+		$sql = "UPDATE {$table} SET user = :user, email = :email WHERE id= :id";
+		$data = Array( ':id' => '24', ':user' => 'user aaa', ':email' => "email aaa" );
+		$update = $this->query ( $sql, $data );
+		return  ( $update->rowCount ( ) > 0 ) ? TRUE : FALSE;
 	}
 
 	# Protetor Singletom na Construção da classe
@@ -67,5 +120,8 @@ class Connect
 	private function __wakeup ( ) { }
 };
 
-$conn = Connect::on ( "test" );
-#var_dump ( $conn );
+$conn = Connect::on ( "callcommunity" );
+#$conn->insert ( "registers", "user, email", "'user1', 'email1 , teste: email.'" );
+#$data = $conn->select ( "registers", "user, email, country" );
+#echo json_encode ( $data );
+var_dump ( $conn->update ( "registers", "user='userAAA', email='emailAAA'", "id='24'" ) );
